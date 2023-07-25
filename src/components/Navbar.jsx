156d21849312eb6d872d02871ec8891a7f32bb98
amarjyothi98 <amarjyothi526@gmail.com>
2023-07-25 20:25:18 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+import { styled } from "@mui/material";
+
+const Header = styled(AppBar)`
+    background: #111111
+`
+const Tabs = styled('p')`
+    font-size: 20px;
+    margin-right: 20px; 
+`
+
 
 export default function Navbar () {
     return (
         <>
-        this is navbar.
+        <Header>
+            <Toolbar>
+                <Tabs>Code For Interview</Tabs>
+                <Tabs>All Users</Tabs>
+                <Tabs>Add Users</Tabs>
+            </Toolbar>
+        </Header>
         </>
     )
 }
