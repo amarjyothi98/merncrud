db51ebe57351ef57b9f46b9846f9cf0a2696565b
amarjyothi98 <amarjyothi526@gmail.com>
2023-08-21 02:27:56 +0530

    for git on ubuntu

--- src/components/CodeForInterview.jsx
+++ src/components/CodeForInterview.jsx
@@ -2,6 +2,8 @@ const CodeForInterview = () => {
     return (
         <>
         this is code for interview
+        <br />
+        text for git, ubuntu!
         {/* <div style={{ height: '100vh', backgroundColor: 'red' }}></div> */}
         </>
     )
